9c2693aa6e06758a6a27907e007a4210ea958aad
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-08-04 11:41:31 +0300

    The grid of cards has been made adaptive

--- src/components/projects/styled.js
+++ src/components/projects/styled.js
@@ -7,7 +7,12 @@ export const StyledProjects = styled.section`
 
 export const StyledProjectsGrid = styled.div`
   display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(360px, 1fr));
   margin-top: 80px;
-  grid-template-columns: 1fr 1fr;
-  gap: 80px;
+  grid-gap: 40px;
+
+  @media (max-width: ${(props) => props.theme.deviceSizes.mobile}){
+    display: flex;
+    flex-direction: column;
+  }
 `;
--- src/theme/defaultTheme.js
+++ src/theme/defaultTheme.js
@@ -6,5 +6,10 @@ export const DefaultTheme = {
     textGray: "#E8E8E8",
     githubButton: "#FFFFFF",
     demoButton: "#EDF738"
+  },
+  deviceSizes: {
+    mobile: "480px",
+    tablet: "780px",
+    laptop: "1240px",
   }
 };
