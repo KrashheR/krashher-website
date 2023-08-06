280e1aec11e07f6b8294747a973d569dc09a128e
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-08-06 16:50:12 +0300

    updated stack component to support new images

--- src/components/stack/stack.jsx
+++ src/components/stack/stack.jsx
@@ -1,24 +1,47 @@
 import React from "react";
-import { StyledStack, StyledStackImg } from "./styled";
-import jsIcon from "../../assets/images/js.png"
-import htmlIcon from "../../assets/images/html.png"
-import sassIcon from "../../assets/images/sass.png"
-import gulpIcon from "../../assets/images/gulp.png"
-import bemIcon from "../../assets/images/bem.png"
+import { StyledStackImg } from "./styled";
+import jsIcon from "../../assets/images/js.png";
+import htmlIcon from "../../assets/images/html.png";
+import scssIcon from "../../assets/images/scss.svg";
+import sassIcon from "../../assets/images/sass.png";
+import gulpIcon from "../../assets/images/gulp.png";
+import bemIcon from "../../assets/images/bem.png";
+import cssIcon from "../../assets/images/css.svg";
+import gitIcon from "../../assets/images/git.svg";
+import reactIcon from "../../assets/images/react.svg";
+import reduxIcon from "../../assets/images/redux.svg";
+import tsIcon from "../../assets/images/ts.svg";
+import vscodeIcon from "../../assets/images/vscode.svg";
+import webpackIcon from "../../assets/images/webpack.svg";
+import figmaIcon from "../../assets/images/figma.png";
+import wordpressIcon from "../../assets/images/wordpress.png";
+import styledComponentsIcon from "../../assets/images/styled-components.png";
 
-function Stack({ stackList }) {
-  const iconList = {"js": jsIcon,
-                    "html":htmlIcon,
+function Stack({ stackList, isTextEnabled }) {
+  const iconList = {"html":htmlIcon,
+                    "css": cssIcon,
                     "sass": sassIcon,
+                    "scss": scssIcon,
+                    "js": jsIcon,
                     "gulp": gulpIcon,
-                    "bem": bemIcon};
+                    "bem": bemIcon,
+                    "git": gitIcon,
+                    "react": reactIcon,
+                    "redux": reduxIcon,
+                    "ts": tsIcon,
+                    "vscode": vscodeIcon,
+                    "webpack": webpackIcon,
+                    "figma": figmaIcon,
+                    "styled-components": styledComponentsIcon,
+                    "wordpress": wordpressIcon,
+                    };
 
   return(
-    <StyledStack>
+    <>
       {stackList.map((item, index) =>(
-        <StyledStackImg src = {iconList[item]} alt={iconList[item]} key={index} />
+        <StyledStackImg src = {iconList[item]} alt={item} key={index} />
       ))}
-    </StyledStack>
+    </>
   );
 }
 
--- src/components/stack/styled.js
+++ src/components/stack/styled.js
@@ -1,17 +1,13 @@
 import styled from "styled-components";
 
-export const StyledStack = styled.div`
-  display: flex;
-  justify-content: center;
-  margin: 20px 0;
-;`
-
 export const StyledStackImg = styled.img`
-  max-height: 50px;
-  max-width: 50px;
+  height: 60px;
+
+  justify-self: center;
 
   @media(max-width: ${(props) => props.theme.deviceSizes.mobile}) {
     max-height: 40px;
     max-width: 40px;
   }
 `;
+
