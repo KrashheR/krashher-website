b1025cb36df23cf3a5f606e2a0ac8e0e52de78cb
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-08-08 17:03:06 +0300

    fixed navbar issues

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { StyledNavToggler, StyledNav } from "./styled";
 import Hamburger from "../hamburger/hamburger";
 import NavigationUl from "../navigationUl/navigationUl";
@@ -6,21 +6,25 @@ import NavigationUl from "../navigationUl/navigationUl";
 
 function Navbar () {
   const [openHamburger, setHamburger] = useState(false);
+  const hamburgerRef = useRef(null);
 
   useEffect(() => {
-    const closeHamburger = ()=> {
+    const closeHamburger = event => {
+      if (hamburgerRef.current && hamburgerRef.current.contains(event.target)) {
+        return;
+      }
       setHamburger(false);
     }
 
     document.addEventListener("mousedown", closeHamburger);
     return () => document.removeEventListener("mousdown", closeHamburger);
-  }, []);
+  }, [openHamburger]);
 
   return (
     <StyledNav>
     <NavigationUl toggle={openHamburger}/>
-    <StyledNavToggler onClick={() => setHamburger(!openHamburger)}>
-      <Hamburger toggle={openHamburger} />
+    <StyledNavToggler ref={hamburgerRef} onClick={() => setHamburger(!openHamburger)}>
+      <Hamburger  toggle={openHamburger} />
     </StyledNavToggler>
   </StyledNav>
   )
